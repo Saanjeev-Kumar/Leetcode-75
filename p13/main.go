5c65d5b72e65bd53b1afb14fe332f10cc41ec0ee
Saanjeev-Kumar <sanjukr16@gmail.com>
2024-11-30 23:47:44 +0530

    added 13th problem

---
+++ p13/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+        "fmt"
+)
+
+func maxOperations(nums []int, k int) int {
+        count := make(map[int]int)
+        operations := 0
+
+        for _, num := range nums {
+                complement := k - num
+                if count[complement] > 0 {
+                        operations++
+                        count[complement]--
+                        count[num]--
+                } else {
+                        count[num]++
+                }
+        }
+
+        return operations
+}
+
+func main() {
+        nums := []int{1, 2, 3, 4}
+        k := 5
+        result := maxOperations(nums, k)
+        fmt.Println(result) // Output: 2
+}
