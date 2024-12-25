c9ad6add450e5b0dd8bcd19a0b4f586c735a4be4
Saanjeev Kumar K R <73130090+Saanjeev-Kumar@users.noreply.github.com>
2024-12-25 13:18:41 +0530

    Update logic to match testcases.go

--- p13/main.go
+++ p13/main.go
@@ -4,6 +4,32 @@ import (
         "fmt"
 )
 
+// func maxOperations(nums []int, k int) int {
+//         count := make(map[int]int)
+//         operations := 0
+
+//         for _, num := range nums {
+//                 complement := k - num
+//                 if count[complement] > 0 {
+//                         operations++
+//                         count[complement]--
+//                         count[num]--
+//                 } else {
+//                         count[num]++
+//                 }
+//         }
+
+//         return operations
+// }
+
+func main() {
+        nums := []int{1, 2, 3, 4}
+        k := 5
+        result := maxOperations(nums, k)
+        fmt.Println(result) // Output: 2
+}
+
+
 func maxOperations(nums []int, k int) int {
         count := make(map[int]int)
         operations := 0
@@ -22,9 +48,3 @@ func maxOperations(nums []int, k int) int {
         return operations
 }
 
-func main() {
-        nums := []int{1, 2, 3, 4}
-        k := 5
-        result := maxOperations(nums, k)
-        fmt.Println(result) // Output: 2
-}
