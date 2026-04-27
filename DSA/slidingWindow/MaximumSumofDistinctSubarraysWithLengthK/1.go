d8addcd8d4ce9cf58410e736392444308afff7cb
SaanjeevKumar <saanjeevkumar@SaanjeevKumars-MacBook-Air.local>
2026-04-27 18:49:00 +0530

    added uniqueElements

--- DSA/slidingWindow/MaximumSumofDistinctSubarraysWithLengthK/1.go
+++ DSA/slidingWindow/MaximumSumofDistinctSubarraysWithLengthK/1.go
@@ -4,11 +4,26 @@ func maximumSubarraySum(nums []int, k int) int64{
     max := win
     // l := n-k
     for i:=0; i< len(nums)-k;i++{
-        win = win - nums[i] + nums[i+k]
-        max = Max(max,win)
+        // win = win - nums[i] + nums[i+k]
+        win1 := nums[i : i+k]
+        if hasUniqueElements(win1) {
+            fmt.Println(win1)
+            max = Max(max,Sum(win1))
+        }
 
     }
     return int64(max)
+}
+
+func hasUniqueElements(arr []int) bool {
+    seen := make(map[int]bool)
+    for _, num := range arr {
+        if seen[num] {
+            return false
+        }
+        seen[num] = true
+    }
+    return true
 }
 
 func Sum(nums []int)int{
